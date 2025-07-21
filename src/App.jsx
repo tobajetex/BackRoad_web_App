6535c9f0e6522ab19f169b802746f409fa3d3d2b
Oloruntoba Jetawo <jetawotobajetex@gmail.com>
2025-07-21 02:50:37 +0100

    import path in App.jsx was change from components/header to components/Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-import Header from "./components/header";
+import Header from "./components/Header";
 import Hero from "./components/hero";
 import About from "./components/About";
 import Services from "./components/Services";
